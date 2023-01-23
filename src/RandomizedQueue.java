94489b8b012870f472026903cee9145add17e1aa
krayzieeddi <63763467+krayzieeddi@users.noreply.github.com>
2023-01-23 14:53:04 -0800

    full doc

--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -1,34 +1,60 @@
 import java.util.Iterator;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Item[] array;
+    private int size;
 
     // construct an empty randomized queue
     public RandomizedQueue() {
+        array = new Item[];
+        size = array.length;
+    }
 
+    private void resize(int capcity) {
+        Item copy[] = new Item[capcity];
+
+        for (int i = 0; i < size; i++) {
+            copy[i] = array[i];
+        }
+
+        array = copy;
     }
 
     // is the randomized queue empty?
     public boolean isEmpty() {
-        return false;
+        if (array.length == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // return the number of items on the randomized queue
     public int size() {
-        return 0;
+        return array.length;
     }
 
     // add the item
     public void enqueue(Item item) {
+        if (isEmpty()) {
+            array = new Item[1];
+            array[0] = item;
+        }
+
+        if (size = array.length) {
+            resize(2 * array.length);
+            array[size++] = item;
+        }
 
     }
 
     // remove and return a random item
-    public Item dequeue() {
+    public Item dequeue(int random) {
         return null;
     }
 
     // return a random item (but do not remove it)
-    public Item sample() {
+    public Item sample(int random) {
         return null;
     }
 
@@ -39,7 +65,11 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // unit testing (required)
     public static void main(String[] args) {
+        RandomizedQueue<String> list = new RandomizedQueue();
 
+        list.enqueue("stuff");
+        list.enqueue("thing");
+        list.enqueue("word");
     }
 
 }
